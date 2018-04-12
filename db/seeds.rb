cce4a61b10122e54fa18ab40ecede60f8c9b2b63
Daniel Ball <g.thirteen333@gmail.com>
2018-04-12 03:40:47 +0000

    Updated business show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,3 +31,34 @@ following = users[2..50]
 followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
+
+
+Business.create!(name:  "Example Business",
+             city:  "Brooklyn",
+             state: "NY",
+             zipcode: "11243",
+             address: "900 7th Ave",
+             phone: "7828899485",
+             email: "example@railstutorial1.org",
+             description: "We do Burgers and Fries")
+
+50.times do |nn|
+  name     = Faker::Name.name
+  city     = Faker::Address.city
+  state    = Faker::Address.state
+  zipcode  = Faker::Address.zip_code
+  address = Faker::Address.street_name
+  phone    = Faker::PhoneNumber.phone_number
+  email    = "example-#{nn+1}@railstutorial2.org"
+  description = Faker::Company.catch_phrase
+  
+  
+  Business.create!(name:        name,
+                   city:        city,
+                   state:       state,
+                   zipcode:     zipcode,
+                   address:    address,
+                   phone:       phone,
+                   email:       email,
+                   description: description)
+end
